776e616a9ee281fe79e339250196ef9b4640af63
Ademir Traesel <ademirtraesel@hotmail.com>
2024-10-27 15:36:57 -0300

    Forms toValue

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -3,3 +3,11 @@
         {...inputs, [name]: {...inputs[name], value: newValue}}
     );
  }
+
+ export function toValues(inputs: any){
+    const data: any = {};
+    for(var name in inputs){
+        data[name] = inputs[name].value;
+    }
+    return data;
+ }
